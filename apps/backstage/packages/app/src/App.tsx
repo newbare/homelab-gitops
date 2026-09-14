a5b0131dff4041efeb9d64895723764bee200470
jefferson.silva <jefferson.silva@spassu.com.bnr>
2026-09-14 18:02:24 -0300

    chore: limpar setup antigo do Backstage para rebuild do zero

--- apps/backstage/packages/app/src/App.tsx
+++ apps/backstage/packages/app/src/App.tsx
@@ -1,8 +1,10 @@
 import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
+import searchPlugin from '@backstage/plugin-search'; // <-- Importa o PLUGIN de busca
 import { navModule } from './modules/nav';
 import { homeModule } from './modules/home';
 
 export default createApp({
-  features: [catalogPlugin, navModule, homeModule],
+  // Adiciona o searchPlugin ao array de features
+  features: [catalogPlugin, searchPlugin, navModule, homeModule],
 });

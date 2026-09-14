9992e546d4ca43eaa4e859723ccffa0b0e52cfa8
jefferson.silva <jefferson.silva@spassu.com.bnr>
2026-09-14 15:14:45 -0300

    Fase 7: remover import do NotificationsSidebarItem + rebuild (tag v3)

--- apps/backstage/packages/app/src/modules/nav/Sidebar.tsx
+++ apps/backstage/packages/app/src/modules/nav/Sidebar.tsx
@@ -12,7 +12,6 @@ import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import { SidebarSearchModal } from '@backstage/plugin-search';
 import { UserSettingsSignInAvatar } from '@backstage/plugin-user-settings';
-import { NotificationsSidebarItem } from '@backstage/plugin-notifications';
 
 export const SidebarContent = NavContentBlueprint.make({
   params: {
@@ -43,7 +42,7 @@ export const SidebarContent = NavContentBlueprint.make({
           </SidebarGroup>
           <SidebarSpace />
           <SidebarDivider />
-          <NotificationsSidebarItem />
+          
           <SidebarDivider />
           <SidebarGroup
             label="Settings"

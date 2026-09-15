14c85e037060113de9c285b84541efe43d45f26f
jefferson.silva <jefferson.silva@spassu.com.bnr>
2026-09-14 23:48:00 -0300

    Fase 7: rebuild com searchPlugin + homePlugin + scaffolderPlugin (imagem v5)

--- apps/backstage/packages/app/src/plugins.ts
+++ apps/backstage/packages/app/src/plugins.ts
@@ -1,1 +1,4 @@
 export { default as notificationsPlugin } from '@backstage/plugin-notifications/alpha';
+export { default as searchPlugin } from '@backstage/plugin-search/alpha';
+export { default as homePlugin } from '@backstage/plugin-home/alpha';
+export { default as scaffolderPlugin } from '@backstage/plugin-scaffolder/alpha';
